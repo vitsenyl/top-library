ce553ea957baecc83655c9fce01ada71acdfb7d8
Yiran Li <v.yiran.li@gmail.com>
2020-06-09 15:18:22 -0700

    Initial commit

---
+++ js/book.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
+    }     
+}
+
+function addBookToLibrary() {
+    
+}
