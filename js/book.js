07db7d33f6a616d9dc3be9e585724415c048df75
Yiran Li <v.yiran.li@gmail.com>
2020-06-09 23:51:06 -0700

    Added ability to add new books

--- js/book.js
+++ js/book.js
@@ -1,7 +1,27 @@
 const libraryShelf = document.querySelector('.library');
+const newBookForm = document.querySelector('#book-info');
+const addBookButton = document.querySelector('#add-book');
 
 let myLibrary = [];
 initializeLibrary();
+
+document.querySelector('#submit').onclick = addBookToLibrary;
+document.querySelector('#cancel').onclick = cancelAdd;
+addBookButton.onclick = displayAddBookForm;
+
+function displayAddBookForm() {
+    if (newBookForm.classList.contains("hidden")) {
+       newBookForm.classList.remove("hidden");
+       newBookForm.reset();
+    } else {
+        newBookForm.classList.add("hidden");
+    }
+    console.log('yes');
+}
+
+function cancelAdd() {
+    displayAddBookForm();
+}
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,9 +30,9 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-Book.prototype.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
-}
+// Book.prototype.info = function() {
+//     return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
+// }
 
 function initializeLibrary() {
     let a = new Book("Divided", "Brian Cornell", 326, "Read");
@@ -28,18 +48,24 @@ function addBookToLibrary() {
     let author = document.querySelector('#book-info #book-author').value;
     let pages = document.querySelector('#book-info #book-pages').value;
     let read = document.querySelector('#book-info #book-read').checked;  
-    
-    let book = new Book(title, author, pages, read ? "Read" : "Not Read");
-    myLibrary.push(book)
+
+    if (title && author && pages) {
+        let book = new Book(title, author, pages, read ? "Read" : "Not Read");
+        myLibrary.push(book);
+        render();
+        newBookForm.reset();
+    }
 }
 
 function render() {
+    libraryShelf.innerHTML = "<h2>Library</h2>";
     myLibrary.forEach(displayBook);
 }
 
 function displayBook(book) {
     let bookDiv = document.createElement("div");
     bookDiv.setAttribute('class', 'book');
+    bookDiv.setAttribute('id', book.title);
     libraryShelf.appendChild(bookDiv);
 
     for (let item in book) {
