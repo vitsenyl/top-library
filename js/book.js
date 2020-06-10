0f7da67e9729b64cb12570b693f2252bdff68ed4
Yiran Li <v.yiran.li@gmail.com>
2020-06-10 16:04:27 -0700

    Refactored to use Reading/Read/Not Read combobox

--- js/book.js
+++ js/book.js
@@ -2,7 +2,7 @@ const libraryShelf = document.querySelector('.library');
 const newBookForm = document.querySelector('#book-info');
 const addBookButton = document.querySelector('#add-book');
 
-let myLibrary = [];
+let myLibrary = []; // Should use hash table ideally... will learn at some point!
 initializeLibrary();
 
 document.querySelector('#submit').onclick = addBookToLibrary;
@@ -35,7 +35,7 @@ function Book(title, author, pages, read) {
 // }
 
 function initializeLibrary() {
-    let a = new Book("Divided", "Brian Cornell", 326, "Read");
+    let a = new Book("Divided", "Brian Cornell", 326, "Reading");
     let b = new Book("Thirst: 2600 Miles to Home", "Heather Anderson,", 208, "Read");
     let c = new Book("Free Outside: A Trek Against Time and Distance", "Jeff Garmire", 264, "Read");
     let d = new Book("The Pursuit of Endurance: Harnessing the Record-Breaking Power of Strength and Resilience", "Jennifer Pharr Davis", 320, "Not Read");
@@ -47,10 +47,10 @@ function addBookToLibrary() {
     let title = document.querySelector('#book-info #book-title').value;
     let author = document.querySelector('#book-info #book-author').value;
     let pages = document.querySelector('#book-info #book-pages').value;
-    let read = document.querySelector('#book-info #book-read').checked;  
+    let read = document.querySelector('#book-info #read-status').value;  
 
     if (title && author && pages) {
-        let book = new Book(title, author, pages, read ? "Read" : "Not Read");
+        let book = new Book(title, author, pages, read);
         myLibrary.push(book);
         render();
         newBookForm.reset();
@@ -65,16 +65,24 @@ function render() {
 function displayBook(book) {
     let bookDiv = document.createElement("div");
     bookDiv.setAttribute('class', 'book');
-    bookDiv.setAttribute('id', book.title);
+    bookDiv.innerHTML = `<span class='title'>${book.title}</span>
+            <span class='author'>${book.author}</span>
+            <span class='pages'>${book.pages} pages</span>
+            <select name="read-status" id="read-status">
+                ${readSelectorOptions(book.read)}
+            </select>
+            <div class='remove-book'></div>
+            <div class='mark-read'></div>`;
+    
     libraryShelf.appendChild(bookDiv);
+}
 
-    for (let item in book) {
-        let span = document.createElement("span");
-        span.setAttribute('class', item);
-        span.textContent = book[item];
-        if (item === "pages") {
-            span.textContent += ' pages';
-        }
-        bookDiv.appendChild(span);
+function readSelectorOptions(readStatus) {
+    let readSelectorString = '';
+    for (let s of ['Read', 'Reading', 'Not Read']) {
+        readSelectorString += `<option value="${s}"`;
+        if (s == readStatus) {readSelectorString += ' selected'};
+        readSelectorString += `>${s}</option><br>`;
     }
+    return readSelectorString;
 }
